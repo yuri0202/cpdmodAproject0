ce86fd8af3ac4fc7b04f051929b5e6ebc80f98ff
yuri0202 <yuriatt0202@gmail.com>
2017-10-08 22:13:07 +0200

    finished first exercise

--- main.c
+++ main.c
@@ -5,38 +5,52 @@
 
 
 /*
- *
- * The program calculates the sum of n randomly generated numbers and shows The algorithm's execution time
- * and its Sp calculated as T(n) / D .. fix
- *
- * argv[1] = n : n numbers to sum
- * argv[2] = maxRandValue : The max integer value for the random number to generate
- *
- * If argv[1] and argv[2] are not supplied, n will be equals to 1000000 and
- * maxRandValue will be equals to 1000
+ * @authors
+ * Yuri Attanasio N97/000259
+ * Danilo Ciano   N97/000260
  *
  *
+ * Il programma calcola la somma di n numeri generati in modo randomico.
+ * Complessità di tempo T(n) = n-1
+ *
+ * Input:
+ * @param n : la quantità di numeri da sommare
+ * @param maxRandValue: il valore intero massimo che il numero random può assumere
+ *
+ * N.B. se argv[1] e argv[2] non sono forniti in fase di esecuzione allora
+ * verranno assunti valori di default: n = 10000000 e maxRandValue = 1000
+ *
+ *
+ * Output:
+ * @param sum = la somma dei numeri
+ * @param D = La durata dell'esecuzione della somma (in microsecondi e secondi)
+ * @param SP = La Sustained Performance calcolata come D/T(n) -
  * The algorithm T(n) = n-1 (Sum of n numbers)
  *
  *
  */
+
+
 int main(int argc, char* argv[]) {
-    /* Variables definition*/
-    int n=0, maxRandValue=0;
+
+    /* Definizione delle variabili */
+    int maxRandValue=0,Tn=0;
+    long int n;
     long sum = 0;
     int i = 0;
-    struct timeval startTime,startFinish;
-    long int duration;
+    struct timeval startTime,finishTime;
+    long int durationMS;
+    long double durationS,SP;
     int *numToSum;
 
 
-    /* Checks argument supplied */
+    /* Controllo sugli argomenti forniti in input dall'utente */
     if(argc > 3) {
         printf("Too many arguments provided");
         exit;
     }
     else if (argc==1) {
-        n = 1000000;
+        n = 10000000;
         maxRandValue=1000;
     }
     else if (argc==2) {
@@ -48,25 +62,34 @@ int main(int argc, char* argv[]) {
         maxRandValue=atoi(argv[2]);
     }
 
-    /* Initializes a random number generator according to current time */
+    Tn=n-1;
+
+    /* Inizializzazione del seme per la generazione di numeri random*/
     srand((unsigned)time(NULL));
 
-    /* Allocate the array and fill with random numbers */
+    /* Allocazione array e riempimento con numeri random */
     numToSum = (int*) malloc (sizeof(int) * n);
     for(i=0;i<n;i++){
         numToSum[i]=rand() % maxRandValue +1;
     }
 
-    gettimeofday(&startTime,0); // The time counter starts
 
-    /* Compute the sum */
+    gettimeofday(&startTime,0); // Inizio calcolo del tempo di esecuzione
+
+    /* Effettuo la somma */
     for (i=0;i<n;i++){
         sum+=numToSum[i];
     }
 
-
-
-
-
+    gettimeofday(&finishTime,0); // Fine calcolo del tempo di esecuzione
+    durationMS = ((finishTime.tv_sec - startTime.tv_sec) * 1000000) +
+            finishTime.tv_usec - startTime.tv_usec; // Calcolo la durata dell'op. somma in microsecondi
+    durationS = ((long double)durationMS)/1000000; // Calcolo la durata dell'op. somma in secondi
+    printf("\nLa somma degli n numeri è: %li\n",sum);
+    printf("\nLa durata in microsecondi dell'operazione di somma è: %li\n",durationMS);
+    printf("\nLa durata in secondi dell'operazione di somma è: %Lf\n",durationS);
+    SP = Tn / durationS; // Calcolo la Sustained Performance
+    printf("\nLa Sustained Performance è: %LF\n",SP);
+    free(numToSum); // Libero l'array
 
 }
